d2254a31384c2ede5b1e63982bf787f61d1861d3
wonhui.ryu <dbdnjsgml9@naver.com>
2019-02-27 16:03:24 +0900

    no message

---
+++ src/test/java/com/matsul/soju/SojuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.matsul.soju;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SojuApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
